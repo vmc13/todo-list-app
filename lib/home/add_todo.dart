882c2849a346afa38283bdee133a0b30c4c62a41
Victória Mendes <vick.costa1305@gmail.com>
2023-08-22 15:38:43 -0300

    Sending task data to firebase

--- lib/home/add_todo.dart
+++ lib/home/add_todo.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AddToDo extends StatefulWidget {
@@ -8,11 +9,14 @@ class AddToDo extends StatefulWidget {
 }
 
 class _AddToDoState extends State<AddToDo> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  String type = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      ),
+      appBar: AppBar(),
       body: SizedBox(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
@@ -48,17 +52,17 @@ class _AddToDoState extends State<AddToDo> {
                 Wrap(
                   runSpacing: 15,
                   children: [
-                    chipData("Important", 0xFFE4572E),
+                    taskSelect("Important", 0xFFE4572E),
                     const SizedBox(width: 20),
-                    chipData("Planned", 0xFF7CDEDC),
+                    taskSelect("Planned", 0xFF7CDEDC),
                     const SizedBox(width: 20),
-                    chipData("My day", 0xFFDE89BE),
+                    taskSelect("My day", 0xFFDE89BE),
                   ],
                 ),
                 const SizedBox(height: 25),
                 label("Description"),
                 const SizedBox(height: 12),
-                inputDescription(),
+                description(),
                 const SizedBox(height: 50),
                 buttonCreateTodo(),
               ],
@@ -72,33 +76,49 @@ class _AddToDoState extends State<AddToDo> {
   // Widgets
 
   Widget buttonCreateTodo() {
-    return Container(
-      height: 56,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        gradient: const LinearGradient(
-          colors: [
-            Color(0xFF9C27B0),
-            Color.fromARGB(255, 124, 17, 143),
-          ],
+    return InkWell(
+      onTap: () async {
+        await FirebaseFirestore.instance.collection("todo").add({
+          "title": _titleController.text,
+          "task": type,
+          "Description": _descriptionController.text
+        });
+        // ignore: use_build_context_synchronously
+        ScaffoldMessenger.of(context).showSnackBar( const SnackBar(
+          content: Text('Task added successfully!'),
+          backgroundColor: Colors.green,
+        ));
+        // ignore: use_build_context_synchronously
+        Navigator.pop(context);
+      },
+      child: Container(
+        height: 56,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: const LinearGradient(
+            colors: [
+              Color(0xFF9C27B0),
+              Color.fromARGB(255, 124, 17, 143),
+            ],
+          ),
         ),
-      ),
-      child: const Center(
-        child: Text(
-          "Add To-Do",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18,
-            fontWeight: FontWeight.w600,
-            fontFamily: 'RobotoMono',
+        child: const Center(
+          child: Text(
+            "Add To-Do",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              fontFamily: 'RobotoMono',
+            ),
           ),
         ),
       ),
     );
   }
 
-  Widget inputDescription() {
+  Widget description() {
     return Container(
       height: 150,
       width: MediaQuery.of(context).size.width,
@@ -107,6 +127,7 @@ class _AddToDoState extends State<AddToDo> {
         borderRadius: BorderRadius.circular(15),
       ),
       child: TextFormField(
+        controller: _descriptionController,
         style: const TextStyle(
           color: Colors.black45,
           fontSize: 17,
@@ -123,23 +144,28 @@ class _AddToDoState extends State<AddToDo> {
     );
   }
 
-  Widget chipData(String label, int color) {
-    return Chip(
-      backgroundColor: Color(color),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      // elevation: 5,
-      // shadowColor: Colors.amber,
-      label: Text(
-        label,
-        style: const TextStyle(
-          color: Colors.white,
-          fontSize: 15,
-          fontWeight: FontWeight.w600,
+  Widget taskSelect(String label, int color) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          type = label;
+        });
+      },
+      child: Chip(
+        backgroundColor: type == label ? Colors.indigoAccent : Color(color),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
         ),
+        label: Text(
+          label,
+          style: TextStyle(
+            color: type == label ? Colors.black : Colors.white,
+            fontSize: 15,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        labelPadding: const EdgeInsets.symmetric(horizontal: 17, vertical: 3.8),
       ),
-      labelPadding: const EdgeInsets.symmetric(horizontal: 17, vertical: 3.8),
     );
   }
 
@@ -152,6 +178,7 @@ class _AddToDoState extends State<AddToDo> {
         borderRadius: BorderRadius.circular(15),
       ),
       child: TextFormField(
+        controller: _titleController,
         style: const TextStyle(
           color: Colors.black45,
           fontSize: 17,
